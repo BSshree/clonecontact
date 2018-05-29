62c14644363425e1eba30f7420a9199a7a06c007
BSshree <34095518+BSshree@users.noreply.github.com>
2018-05-29 16:43:05 +0530

    #comment css changes

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -13,6 +13,7 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'css/site.css',
+         'https://use.fontawesome.com/releases/v5.0.6/css/all.css',
     ];
     public $js = [
     ];
